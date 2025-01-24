b399a01d26d88b2927aa967fd71cf897388ba247
bautista baiocchi lora <bautistabaiocchilora@gmail.com>
2025-01-24 13:36:33 -0300

    add: damm social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,7 @@ const config: Config = {
   },
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/damm-social-card.png',
     navbar: {
       title: 'DAMM Capital',
       logo: {
